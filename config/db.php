b354e9b8a0cb5cd4cedf4e013a66acc2ad0731ed
mugabus <fatakimugaruka91@gmail.com>
2024-10-21 23:27:08 +0200

    connexion db

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+$host = '127.0.0.1'; // or 'localhost'
+$db = 'water_quality_db';
+$user = 'root'; // or your MySQL username
+$pass = ''; // your MySQL password if any
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
